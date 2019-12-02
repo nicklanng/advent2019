d455ff82891c002b3b4886a32c7542116446f8b2
Nick Lanng <rinse@rinseworld.com>
2019-12-02 16:15:17 +0000

    Add files via upload

---
+++ 1_2/main_test.go
@@ -0,0 +1,25 @@
+package main
+
+import "testing"
+
+func TestCalculateFuelRequirements(t *testing.T) {
+	if result := calculateFuelRequirements(12); result != 2 {
+		t.Errorf("Incorrect fuel requirement\nExpected %d\nGot%d\n", 2, result)
+	}
+
+	if result := calculateFuelRequirements(14); result != 2 {
+		t.Errorf("Incorrect fuel requirement\nExpected %d\nGot%d\n", 2, result)
+	}
+
+	if result := calculateFuelRequirements(1969); result != 654 {
+		t.Errorf("Incorrect fuel requirement\nExpected %d\nGot%d\n", 654, result)
+	}
+
+	if result := calculateFuelRequirements(100756); result != 33583 {
+		t.Errorf("Incorrect fuel requirement\nExpected %d\nGot%d\n", 33583, result)
+	}
+
+	if result := calculateFuelRequirements(1); result != 0 {
+		t.Errorf("Incorrect fuel requirement\nExpected %d\nGot%d\n", 0, result)
+	}
+}
