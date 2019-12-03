699877646ffdcf9317c6cbc314af1299562d6992
Nick Lanng <rinse@rinseworld.com>
2019-12-03 10:34:27 +0000

    Add files via upload

---
+++ 3_1/main.go
@@ -0,0 +1,178 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"math"
+	"strconv"
+	"strings"
+)
+
+type Orientation byte
+
+const (
+	Horizontal Orientation = 0
+	Vertical Orientation = 1
+)
+
+type Vector2 struct {
+	X int
+	Y int
+}
+
+type Line struct {
+	Start Vector2
+	End Vector2
+}
+
+func (l Line) Orientation() Orientation {
+	if l.Start.X != l.End.X {
+		return Horizontal
+	}
+	return Vertical
+}
+
+func (l Line) Intersects(l2 Line) bool {
+	if l.Orientation() == l2.Orientation() {
+		return false
+	}
+
+
+	if l.Orientation() == Horizontal {
+		// line 1 is left of line 2
+		if l.Start.X < l2.Start.X && l.End.X < l2.Start.X {
+			return false
+		}
+
+		// line 2 is right of line 2
+		if l.Start.X > l2.Start.X && l.End.X > l2.Start.X {
+			return false
+		}
+
+		//is line above or below line 2
+		if l.Start.Y < l2.Start.Y || l.Start.Y > l2.End.Y {
+			return false
+		}
+
+		return true
+
+	} else {
+		// line 1 is below line 2
+		if l.Start.Y < l2.Start.Y && l.End.Y < l2.Start.Y {
+			return false
+		}
+
+		// line 2 is above line 2
+		if l.Start.Y > l2.Start.Y && l.End.Y > l2.Start.Y {
+			return false
+		}
+
+		// is line to left or right of line 2
+		if l.Start.X < l2.Start.X || l.Start.X > l2.End.X {
+			return false
+		}
+
+		return true
+	}
+}
+
+type Path struct {
+	Lines []Line
+}
+
+func main() {
+	b, err := ioutil.ReadFile("input.txt")
+	if err != nil {
+		log.Fatalf("failed to load input: %v\n", err)
+	}
+
+	inputRows := strings.Split(string(b), "\n")
+	paths := parsePaths(inputRows)
+	collisions := PlotAndMeasure(paths[0], paths[1])
+
+	for i := range collisions {
+		fmt.Println(collisions[i], math.Abs(float64(collisions[i].X)) + math.Abs(float64(collisions[i].Y)))
+	}
+}
+
+func parsePaths(inputRows []string) []Path {
+	var result []Path
+
+	for i := range inputRows {
+		var currentPosition Vector2
+		var path Path
+
+
+		if len(inputRows[i]) == 0 {
+			continue
+		}
+
+		tokens := strings.Split(inputRows[i], ",")
+		for _, t := range tokens {
+
+			command := t[0]
+			dist, err := strconv.Atoi(t[1:])
+			if err != nil {
+				panic(err)
+			}
+
+			l := Line{Start: currentPosition}
+
+			switch command {
+			case 'U':
+				currentPosition.Y += dist
+				break
+			case 'D':
+				currentPosition.Y -= dist
+				break
+			case 'R':
+				currentPosition.X += dist
+				break
+			case 'L':
+				currentPosition.X -= dist
+				break
+			}
+
+			l.End = currentPosition
+
+			if l.Orientation() == Horizontal && l.Start.X > l.End.X {
+				l.Start, l.End = l.End, l.Start
+			}
+
+			if l.Orientation() == Vertical && l.Start.Y > l.End.Y {
+				l.Start, l.End = l.End, l.Start
+			}
+
+			path.Lines = append(path.Lines, l)
+		}
+
+		result = append(result, path)
+	}
+
+	return result
+}
+
+func PlotAndMeasure(path1, path2 Path) []Vector2 {
+	var result []Vector2
+
+	for _, l1 := range path1.Lines {
+		for _, l2 := range path2.Lines {
+			if l1.Intersects(l2) {
+				if l1.Orientation() == Horizontal {
+					result = append(result, Vector2{
+						X: l2.Start.X,
+						Y: l1.Start.Y,
+					})
+				} else {
+					result = append(result, Vector2{
+						X: l1.Start.X,
+						Y: l2.Start.Y,
+					})
+				}
+			}
+		}
+	}
+
+	return result
+}
